3b04ae137e06cfed955fd8a2dd121128ad8175d6
Yanis Rezgui <chayano57@gmail.com>
2023-04-05 13:25:01 +0200

    connexion deconnexion lieux

--- src/components/signIn.vue
+++ src/components/signIn.vue
@@ -3,19 +3,19 @@
         <h1>Connexion</h1>
         <form @submit.prevent="submitForm">
             <div>
-            <label>
-                Email:<br>
-                <input type="email" id="email" name="email" v-model="email" required>
-                <div v-if="emailError" class="error-message">{{ emailError }}</div>
+                <label>
+                    Email:<br>
+                    <input type="email" id="email" name="email" v-model="email" required>
+                    <div v-if="emailError" class="error-message">{{ emailError }}</div>
 
-            </label>
+                </label>
             </div>
             <div>
-            <label>
-                Mot de passe:<br>
-                <input type="password" id="password" name="password" v-model="password" required>
-                <div v-if="passwordError" class="error-message">{{ passwordError }}</div>
-            </label>
+                <label>
+                    Mot de passe:<br>
+                    <input type="password" id="password" name="password" v-model="password" required>
+                    <div v-if="passwordError" class="error-message">{{ passwordError }}</div>
+                </label>
             </div>
             <button type="submit" class="buttonLog">Se connecter</button>
             <p v-if="error" class="error-message">{{ error }}</p>
@@ -65,6 +65,7 @@ export default {
                         this.$store.state.username = response.data.user;
                         this.$store.state.tel_number = response.data.tel;
                         this.$store.state.token = response.data.accesstoken;
+                        this.$store.commit('login')
                         this.$router.push('/homePage')
 
                     }
@@ -95,55 +96,60 @@ export default {
 </script>
   
 <style>
+body {
+    background-color: #D7D7D7
+}
 
-    body {
-        background-color: #D7D7D7
-    }
-    .container-connexion {
-        justify-content: center;
-        text-align: center;
-        margin: 100px auto;
-        background-color: #FFFFFF;
-        border-radius: 10px;
-        padding:40px;
-        height: 250px;
-        width:250px;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;     
-    }
-    .container-connexion h1 {
-        font-size: 2rem;
-        font-weight: bold;
-        margin-bottom: 20px;
-        color: hsl(0, 0%, 8%);
-    }
-    form {
-        display: block;
-    }
-    form div {
-        margin-top: 20px;
-    }
-    .buttonLog {
-        background-color: #F0F2F5;
-        border: 1px solid #F0F2F5;
-        border-radius: 5px;
-        color: #000000;
-        padding: 10px 20px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 35px 0 10px 0;
-        cursor: pointer;
-        background-color: rgb(112, 112, 201);
-        color: #ebebeb;
-    }
-    .buttonLog:hover {
-        background-color: rgb(68, 68, 218);
-        color: #ebebeb;
-    }
-    .error-message {
-        color: red;
-        font-size: 0.8rem;
-    }
+.container-connexion {
+    justify-content: center;
+    text-align: center;
+    margin: 100px auto;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    padding: 40px;
+    height: 250px;
+    width: 250px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+}
 
+.container-connexion h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 20px;
+    color: hsl(0, 0%, 8%);
+}
+
+form {
+    display: block;
+}
+
+form div {
+    margin-top: 20px;
+}
+
+.buttonLog {
+    background-color: #F0F2F5;
+    border: 1px solid #F0F2F5;
+    border-radius: 5px;
+    color: #000000;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 35px 0 10px 0;
+    cursor: pointer;
+    background-color: rgb(112, 112, 201);
+    color: #ebebeb;
+}
+
+.buttonLog:hover {
+    background-color: rgb(68, 68, 218);
+    color: #ebebeb;
+}
+
+.error-message {
+    color: red;
+    font-size: 0.8rem;
+}
 </style>
